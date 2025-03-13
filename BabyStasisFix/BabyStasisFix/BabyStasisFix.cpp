e82ff85b7219b56701c69c5ef7ebd3566833f878
Dave <1156232+Lacoi@users.noreply.github.com>
2025-03-13 21:43:20 +0100

    add BabyStasisFix Plugin
    This plugin prevent the dino grow progress loose on server restart. It's lets also grow the babies constantly

---
+++ BabyStasisFix/BabyStasisFix/BabyStasisFix.cpp
@@ -0,0 +1,93 @@
+#include <API/Base.h>
+#include <API/ARK/Ark.h>
+#include "json.hpp"
+#include <fstream>
+
+#pragma comment(lib, "AsaApi.lib")
+
+nlohmann::json config;
+std::chrono::system_clock::time_point nextRun;
+
+void UnstasisBabies()
+{
+    const auto now = std::chrono::system_clock::now();
+    if (now >= nextRun)
+    {
+        int counter = 0;
+
+        nextRun = now + std::chrono::seconds(config["tick"].get<int>());
+
+        TArray<AActor*> actors;
+        UGameplayStatics::GetAllActorsOfClass(AsaApi::GetApiUtils().GetWorld(), APrimalDinoCharacter::GetPrivateStaticClass(), &actors);
+        for (AActor* actor : actors)
+        {
+            if (actor == nullptr)
+                continue;
+
+            const int team_id = actor->TargetingTeamField();
+            if (team_id < 50000)
+                continue;
+
+            APrimalDinoCharacter* dino = static_cast<APrimalDinoCharacter*>(actor);
+            if (!dino->bIsBaby()())
+                continue;
+
+            if (dino->bStasised()()) {
+                counter++;
+                dino->Unstasis();
+            }
+        }
+        Log::GetLog()->info("Update tick for {} babies. Next tick in {} seconds.", counter, config["tick"].get<int>());
+    }
+}
+
+void ReadConfig()
+{
+    const std::string config_path = AsaApi::Tools::GetCurrentDir() + "/ArkApi/Plugins/BabyStasisFix/config.json";
+    std::ifstream file{ config_path };
+    if (!file.is_open())
+        throw std::runtime_error("Can't open config.json");
+
+    file >> config;
+
+    file.close();
+}
+
+
+// Called when the server is ready, do post-"server ready" initialization here
+void OnServerReady()
+{
+    nextRun = std::chrono::system_clock::now();
+
+    AsaApi::GetCommands().AddOnTimerCallback("UnstasisBabies", &UnstasisBabies);
+}
+
+// Hook that triggers once when the server is ready
+DECLARE_HOOK(AShooterGameMode_BeginPlay, void, AShooterGameMode*);
+void Hook_AShooterGameMode_BeginPlay(AShooterGameMode* _this)
+{
+    AShooterGameMode_BeginPlay_original(_this);
+
+    OnServerReady();
+}
+
+// Called by AsaApi when the plugin is loaded, do pre-"server ready" initialization here
+extern "C" __declspec(dllexport) void Plugin_Init()
+{
+    Log::Get().Init("BabyStasisFix");
+
+    ReadConfig();
+
+    AsaApi::GetHooks().SetHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay,
+        &AShooterGameMode_BeginPlay_original);
+
+    if (AsaApi::GetApiUtils().GetStatus() == AsaApi::ServerStatus::Ready)
+        OnServerReady();
+}
+
+// Called by AsaApi when the plugin is unloaded, do cleanup here
+extern "C" __declspec(dllexport) void Plugin_Unload()
+{
+    AsaApi::GetHooks().DisableHook("AShooterGameMode.BeginPlay()", Hook_AShooterGameMode_BeginPlay);
+    AsaApi::GetCommands().RemoveOnTimerCallback("UnstasisBabies");
+}
